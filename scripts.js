d5bfa394db5cee18f12a7767cd3bc71f9b13b498
Joselmo da Silva <121970575+joselmosilva@users.noreply.github.com>
2023-03-19 08:34:42 -0300

    atualizacao

---
+++ scripts.js
@@ -0,0 +1,10 @@
+
+function trocacor(cor){
+    let circulo = document.querySelector(".circulo")
+    console. log (circulo)
+    circulo.style.background = cor
+}
+function trocaimagem(imagem){
+    let celular14 = document.querySelector(".celular14")
+    celular14.src = imagem
+}
